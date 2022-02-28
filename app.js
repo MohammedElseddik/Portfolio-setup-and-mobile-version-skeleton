476c1f54585f7f8c62399c590bd238b088cb0e73
Virag Kormoczy <vkormoczy@gmail.com>
2022-02-28 19:04:12 +0200

    Add mobile menu list

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const firstBar = document.querySelector('.first-bar');
-const secondBar = document.querySelector('.second-bar');
-const thirdBar = document.querySelector('.third-bar');
-const bar = document.querySelectorAll('.bar');
-const navBar = document.querySelector('.nav-bar');
-const mainHeader = document.querySelector('header');
-const mylogo = document.querySelector('.logo');
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const firstBar = document.querySelector(".first-bar");
+const secondBar = document.querySelector(".second-bar");
+const thirdBar = document.querySelector(".third-bar");
+const bar = document.querySelectorAll(".bar");
+const navBar = document.querySelector(".nav-bar");
+const mainHeader = document.querySelector("header");
+const mylogo = document.querySelector(".logo");
+const mainBarItems = document.querySelector(".main-bar__items");
 
-
-hamburgerMenu.addEventListener('click',()=>{
-    navBar.classList.toggle('expand');
-    mainHeader.classList.toggle('main-header');
-    hamburgerMenu.classList.toggle('mobile-hamburger-menu');
-    mylogo.classList.toggle('mobile-logo')
-    bar.forEach((elem)=>{
-        elem.classList.toggle('bar-color');
-    })
-    secondBar.classList.toggle('hidden');
-    firstBar.classList.toggle('tronsform');
-    thirdBar.classList.toggle('tronsform');
-})
+hamburgerMenu.addEventListener("click", () => {
+  navBar.classList.toggle("expand");
+  mainHeader.classList.toggle("main-header");
+  hamburgerMenu.classList.toggle("mobile-hamburger-menu");
+  mylogo.classList.toggle("mobile-logo");
+  bar.forEach((elem) => {
+    elem.classList.toggle("bar-color");
+  });
+  secondBar.classList.toggle("hidden");
+  firstBar.classList.toggle("tronsform");
+  thirdBar.classList.toggle("tronsform");
+  mainBarItems.classList.toggle("main-bar__items-visible");
+});
