f64b33420bc826d32e87b16bbd9fe749dc253601
MohammedElseddik <rebbouh.mohammed.elseddik@gmail.com>
2022-02-28 16:48:30 +0100

    Add mobile menu

---
+++ app.js
@@ -0,0 +1,22 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const firstBar = document.querySelector('.first-bar');
+const secondBar = document.querySelector('.second-bar');
+const thirdBar = document.querySelector('.third-bar');
+const bar = document.querySelectorAll('.bar');
+const navBar = document.querySelector('.nav-bar');
+const mainHeader = document.querySelector('header');
+const mylogo = document.querySelector('.logo');
+
+
+hamburgerMenu.addEventListener('click',()=>{
+    navBar.classList.toggle('expand');
+    mainHeader.classList.toggle('main-header');
+    hamburgerMenu.classList.toggle('mobile-hamburger-menu');
+    mylogo.classList.toggle('mobile-logo')
+    bar.forEach((elem)=>{
+        elem.classList.toggle('bar-color');
+    })
+    secondBar.classList.toggle('hidden');
+    firstBar.classList.toggle('tronsform');
+    thirdBar.classList.toggle('tronsform');
+})
