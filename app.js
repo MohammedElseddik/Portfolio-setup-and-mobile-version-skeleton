ae0e1a1e6a8da740c327b17d32379951e7a4197c
MohammedElseddik <rebbouh.mohammed.elseddik@gmail.com>
2022-02-28 23:50:15 +0100

    Ass style to mobile menu

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
+const bar = document.querySelectorAll(".bar");
 const firstBar = document.querySelector(".first-bar");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const mainBarItems = document.querySelector(".main-bar__items");
+const mylogo = document.querySelector(".logo");
+const navBar = document.querySelector(".nav-bar");
 const secondBar = document.querySelector(".second-bar");
 const thirdBar = document.querySelector(".third-bar");
-const bar = document.querySelectorAll(".bar");
-const navBar = document.querySelector(".nav-bar");
-const mainHeader = document.querySelector("header");
-const mylogo = document.querySelector(".logo");
-const mainBarItems = document.querySelector(".main-bar__items");
 
-hamburgerMenu.addEventListener("click", () => {
-  navBar.classList.toggle("expand");
-  mainHeader.classList.toggle("main-header");
-  hamburgerMenu.classList.toggle("mobile-hamburger-menu");
-  mylogo.classList.toggle("mobile-logo");
+const applyStyles = function() {
   bar.forEach((elem) => {
     elem.classList.toggle("bar-color");
   });
-  secondBar.classList.toggle("hidden");
-  firstBar.classList.toggle("tronsform");
-  thirdBar.classList.toggle("tronsform");
+  document.body.classList.toggle('overlay');
+  firstBar.classList.toggle("transform");
+  hamburgerMenu.classList.toggle("mobile-hamburger-menu");
   mainBarItems.classList.toggle("main-bar__items-visible");
-});
+  mylogo.classList.toggle("mobile-logo");
+  navBar.classList.toggle("expand-navbar");
+  secondBar.classList.toggle("hidden");
+  thirdBar.classList.toggle("transform");
+};
+
+['click', 'keypress'].forEach(event => {hamburgerMenu.addEventListener(event, applyStyles)});
