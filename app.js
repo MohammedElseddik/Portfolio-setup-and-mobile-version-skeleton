76eefa39e33476fab42b12411e46666eb27d0167
MohammedElseddik <rebbouh.mohammed.elseddik@gmail.com>
2022-03-01 00:02:04 +0100

    Fix linters errors

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const bar = document.querySelectorAll(".bar");
-const firstBar = document.querySelector(".first-bar");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const mainBarItems = document.querySelector(".main-bar__items");
-const mylogo = document.querySelector(".logo");
-const navBar = document.querySelector(".nav-bar");
-const secondBar = document.querySelector(".second-bar");
-const thirdBar = document.querySelector(".third-bar");
+const bar = document.querySelectorAll('.bar');
+const firstBar = document.querySelector('.first-bar');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const mainBarItems = document.querySelector('.main-bar__items');
+const mylogo = document.querySelector('.logo');
+const navBar = document.querySelector('.nav-bar');
+const secondBar = document.querySelector('.second-bar');
+const thirdBar = document.querySelector('.third-bar');
 
-const applyStyles = function () {
+const applyStyles = function() {
   bar.forEach((elem) => {
-    elem.classList.toggle("bar-color");
+    elem.classList.toggle('bar-color');
   });
-  document.body.classList.toggle("overlay");
-  firstBar.classList.toggle("transform");
-  hamburgerMenu.classList.toggle("mobile-hamburger-menu");
-  mainBarItems.classList.toggle("main-bar__items-visible");
-  mylogo.classList.toggle("mobile-logo");
-  navBar.classList.toggle("expand-navbar");
-  secondBar.classList.toggle("hidden");
-  thirdBar.classList.toggle("transform");
+  document.body.classList.toggle('overlay');
+  firstBar.classList.toggle('transform');
+  hamburgerMenu.classList.toggle('mobile-hamburger-menu');
+  mainBarItems.classList.toggle('main-bar__items-visible');
+  mylogo.classList.toggle('mobile-logo');
+  navBar.classList.toggle('expand-navbar');
+  secondBar.classList.toggle('hidden');
+  thirdBar.classList.toggle('transform');
 };
 
-["click", "keypress"].forEach((event) => {
-  hamburgerMenu.addEventListener(event, applyStyles);
-});
+['click', 'keypress'].forEach(event => {hamburgerMenu.addEventListener(event, applyStyles)});
